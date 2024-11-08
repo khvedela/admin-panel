f18a4e8954f8bbeeb2f6485ea2b7aadd12fd6b44
David Khvedelidze <d.khv08@gmail.com>
2024-11-08 18:40:11 +0100

    added primeng and tailwind to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PrimeNGConfig} from 'primeng/api';
+import {Aura} from 'primeng/themes/aura';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'admin-panel';
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({
+      preset: Aura,
+      options: {
+        prefix: 'p',
+        darkModeSelector: '.my-app-dark',
+        cssLayer: false
+      }
+    })
+  }
 }
